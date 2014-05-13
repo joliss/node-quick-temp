b96983565655e0cab8d3313f332b68a06e437892
Stefan Penner <stefan.penner@gmail.com>
2014-05-13 01:27:56 -0400

    as the try/catch deopts, we may as well keep the cheap conditional in the parent function.

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ function remove(obj, prop) {
 
 
 function makeTmpDir(name, prop) {
-  findBaseDir()
+  if (baseDir == null) {
+    findBaseDir()
+  }
   var tmpDirName = prettyTmpDirName(name, prop)
   return mktemp.createDirSync(path.join(baseDir, tmpDirName))
 }
@@ -38,18 +40,16 @@ function makeTmpDir(name, prop) {
 var baseDir
 
 function findBaseDir () {
-  if (baseDir == null) {
-    try {
-      if (fs.statSync('tmp').isDirectory()) {
-        baseDir = fs.realpathSync('tmp')
-      }
-    } catch (err) {
-      if (err.code !== 'ENOENT') throw err
+  try {
+    if (fs.statSync('tmp').isDirectory()) {
+      baseDir = fs.realpathSync('tmp')
+    }
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err
       // We could try other directories, but for now we just create ./tmp if
       // it doesn't exist
       fs.mkdirSync('tmp')
       baseDir = fs.realpathSync('tmp')
-    }
   }
 }
 
